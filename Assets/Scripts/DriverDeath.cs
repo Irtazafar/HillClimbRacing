0987878cd5ff272b75d15426beab6e30eb2c59e5
Irtazafar <irtaza.fast@gmail.com>
2023-08-09 16:16:50 +0500

    Bone Cracking Sound
    Bone cracking sound is added in the game.

--- Assets/Scripts/DriverDeath.cs
+++ Assets/Scripts/DriverDeath.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class DriverDeath : MonoBehaviour
 {
+    [SerializeField] private AudioSource sound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
+            sound.Play();
             GameManager.instance.GameOver();
+            //sound.Stop();
         }
     }
 }
