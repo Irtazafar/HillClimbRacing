92505ee5d7b5d7a36bf4f12fd28b3dd4b9a56aa1
Irtazafar <irtaza.fast@gmail.com>
2023-08-09 16:32:04 +0500

    Fuel Collection Effect
    Fuel Collection effect is added, as when the player collects the fuel a effect is shown

--- Assets/Scripts/FuelCollect.cs
+++ Assets/Scripts/FuelCollect.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class FuelCollect : MonoBehaviour
 {
+    [SerializeField]
+    GameObject effect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             FuelController.instance.FillFuel();
             Destroy(gameObject);
+            Instantiate(effect, transform.position, Quaternion.identity);
         }
     }
 }
